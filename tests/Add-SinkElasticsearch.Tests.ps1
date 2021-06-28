812f93789ae12a0e694168124b9eec03304bf2c5
Martin Dennhardt <martin.dennhardt.ext@siemens-healthineers.com>
2021-06-28 11:55:11 +0000

    "Fix" test

--- tests/Add-SinkElasticsearch.Tests.ps1
+++ tests/Add-SinkElasticsearch.Tests.ps1
@@ -1,3 +1,6 @@
+BeforeAll {
+    Import-Module ../src/PoShLog.Sinks.Elasticsearch.psd1
+}
 Describe 'Add-SinkElasticsearch' {
     It 'Adds an elasticsearch sink, that can be successfully written to' {
         New-Logger |
@@ -12,7 +15,7 @@ Describe 'Add-SinkElasticsearch' {
         Write-InformationLog '{level} message for test {guid}' -PropertyValues 'Information', $guid
 
         Close-Logger
-        Start-Sleep 1 # ouchi!
+        Start-Sleep 3 # ouchi!
 
         $response = Invoke-WebRequest -Uri "http://elasticsearch:9200/pester/_search?q=fields.guid:'$guid'"
         ($response.Content | ConvertFrom-Json).hits.total.value | Should -Be 1
